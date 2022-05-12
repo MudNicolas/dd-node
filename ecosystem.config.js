0dd116907703bd885daca242f4c9c63d7eaeb332
Nicolas <1075039737@qq.com>
2022-05-12 21:28:34 +0800

    config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  apps: [{
-    name: 'DD@Home Nodejs',
-    script: 'index.js',
-    instances: 1,
-    autorestart: true,
-    watch: false
-  }]
+    apps: [
+        {
+            name: "DD@Home Nodejs",
+            script: "index.js",
+            instances: 1,
+            error_file: "./logs/error.log",
+            out_file: "./logs/out.log",
+            log_date_format: "YYYY-MM-DD HH:mm:ss",
+            autorestart: true,
+            watch: ["index.js"],
+            env: {
+                VERBOSE: true,
+                URL: "wss://cluster.vtbs.moe/?runtime=electronv15.3.0&version=1.13.0&platform=win32&uuid=9d44e19b-1afd-47bf-9c1e-39e16bcc14e6&name=Nicolas",
+            },
+        },
+    ],
 }
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
-const { version: VERSION } = require('./package.json')
-const { URL } = require('url') // Compatibility
-const DDAtHome = require('./core')
-const neodoc = require('neodoc')
+const { version: VERSION } = require("./package.json")
+const { URL } = require("url") // Compatibility
+const DDAtHome = require("./core")
+const neodoc = require("neodoc")
 
-const args = neodoc.run(`Start DD@Home client.
+const args = neodoc.run(
+    `Start DD@Home client.
 
 Usage:
   DDatHome-nodejs [options]
@@ -25,77 +26,84 @@ Options:
                      
   --nickname=<name>  Use a nickname. [env: NICKNAME]
   --verbose          Be more verbose. [env: VERBOSE]
-`, { version: VERSION })
+`,
+    { version: VERSION }
+)
 
 if (process.env.development) {
-  console.log('Development Environment Detected')
+    console.log("Development Environment Detected")
 }
 
+console.log(args)
+
 start({
-  url: process.env.development ? 'ws://0.0.0.0:9013' : args['--url'],
-  interval: args['--interval'],
-  anonymous: args['--anonymous'],
-  nickname: args['--nickname'],
-  limit: args['--ws-limit'],
-  verbose: process.env.development || args['--verbose']
+    url: process.env.development ? "ws://0.0.0.0:9013" : args["--url"],
+    interval: args["--interval"],
+    anonymous: args["--anonymous"],
+    nickname: args["--nickname"],
+    limit: args["--ws-limit"],
+    verbose: process.env.development || args["--verbose"],
 })
 
 function start({
-  url,
-  interval = 480,
-  anonymous = false,
-  nickname,
-  limit = Infinity,
-  verbose = false
+    url,
+    interval = 480,
+    anonymous = false,
+    nickname,
+    limit = Infinity,
+    verbose = false,
 }) {
-  console.log(welcome() + '\n')
+    console.log(welcome() + "\n")
 
-  url = new URL(url)
+    url = new URL(url)
 
-  // input such as `false`, `''`
-  if ((typeof interval !== 'number' && !interval) || isNaN(Number(interval))) {
-    throw new TypeError(`interval is not a number: ${interval}`)
-  }
-  if ((typeof limit !== 'number' && !limit) || isNaN(Number(limit))) {
-    throw new TypeError(`limit is not a number: ${limit}`)
-  }
-
-  // env values might be string
-  if (typeof interval === 'string') {
-    interval = Number(interval)
-  }
-
-  if (!anonymous) {
-    url.searchParams.set('runtime', `node${process.version}`)
-    url.searchParams.set('version', VERSION)
-    url.searchParams.set('platform', process.platform)
-    if (process.env.docker) {
-      url.searchParams.set('docker', 'docker')
+    // input such as `false`, `''`
+    if (
+        (typeof interval !== "number" && !interval) ||
+        isNaN(Number(interval))
+    ) {
+        throw new TypeError(`interval is not a number: ${interval}`)
     }
-  }
+    if ((typeof limit !== "number" && !limit) || isNaN(Number(limit))) {
+        throw new TypeError(`limit is not a number: ${limit}`)
+    }
 
-  if (nickname) {
-    url.searchParams.set('name', nickname)
-  }
+    // env values might be string
+    if (typeof interval === "string") {
+        interval = Number(interval)
+    }
 
-  console.log({
-    INTERVAL: interval,
-    limit,
-    verbose
-  })
-  console.log(`using: ${url}\n`)
+    if (!anonymous) {
+        url.searchParams.set("runtime", `node${process.version}`)
+        url.searchParams.set("version", VERSION)
+        url.searchParams.set("platform", process.platform)
+        if (process.env.docker) {
+            url.searchParams.set("docker", "docker")
+        }
+    }
 
-  const ws = new DDAtHome(url, { INTERVAL: interval, wsLimit: limit })
+    if (nickname) {
+        url.searchParams.set("name", nickname)
+    }
 
-  if (verbose) {
-    ws.on('log', console.log)
-  }
+    console.log({
+        INTERVAL: interval,
+        limit,
+        verbose,
+    })
+    console.log(`using: ${url}\n`)
+
+    const ws = new DDAtHome(url, { INTERVAL: interval, wsLimit: limit })
+
+    if (verbose) {
+        ws.on("log", console.log)
+    }
 }
 
 function welcome() {
-  return `${'D'.repeat(Math.max(10, process.stdout.columns - 1))}
+    return `${"D".repeat(Math.max(10, process.stdout.columns - 1))}
 Thank you for participating DD@Home,
 Please read README.md for more information;
 Type -h for command line options.
-${'D'.repeat(Math.max(10, process.stdout.columns - 1))}`
+${"D".repeat(Math.max(10, process.stdout.columns - 1))}`
 }
